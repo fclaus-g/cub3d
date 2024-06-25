a79b8efefb0123f7584825660e63d6cbee16c8d6
fclaus-g <fclaus-g@student.42malaga.com>
2024-06-25 15:06:57 +0200

    wip: draw the 3d

--- src/2paint_minimap.c
+++ src/2paint_minimap.c
@@ -76,13 +76,92 @@ void ft_draw_ray(t_game *cub)
 {
 	int x;
 	int y;
+	double start_ray;
+	double end_ray;
+	double vision_angle;
 
-	x = cub->player.x + PLAYER_MINIMAP_SIZE / 2;
-	y = cub->player.y + PLAYER_MINIMAP_SIZE / 2;
-	while (cub->map.map[y / PIX][x / PIX] != '1')
+	printf("Dibujando rayos\n");
+	vision_angle = M_PI / 3;
+	printf ("vision_angle: %f\n", vision_angle);
+	start_ray = cub->player.angle - vision_angle / 2;
+	printf("start_ray: %f\n", start_ray);
+	end_ray = cub->player.angle + vision_angle / 2;
+	printf("end_ray: %f\n", end_ray);
+
+	while (start_ray < end_ray)
 	{
-		mlx_put_pixel(cub->mini, x / 4, y / 4, 0x00ff00ff);
-		x += sin(cub->player.angle) * 4;
-		y -= cos(cub->player.angle) * 4;
+		//printf("start_ray: %f\n", start_ray);
+		x = cub->player.x + PLAYER_MINIMAP_SIZE / 2;
+		y = cub->player.y + PLAYER_MINIMAP_SIZE / 2;
+		while (cub->map.map[y / PIX][x / PIX] != '1')
+		{
+
+			mlx_put_pixel(cub->mini, x / 4, y / 4, 0x00ff00ff);
+			x += sin(start_ray) * 4;
+			y -= cos(start_ray) * 4;
+		}
+		ft_draw_view(cub);
+		start_ray += 0.01;
 	}
+	
 }
+
+void ft_draw_view(t_game *cub)
+{
+	int start_y;
+	int end_y;
+	int mid_y;
+	int count;
+
+	count = 0;
+	mid_y = cub->map.h_px / 2;
+	start_y = mid_y - cub->map.h_px / 2;
+	end_y = mid_y + cub->map.h_px / 2;
+	count = start_y;
+	while (count < start_y)
+	{
+		mlx_put_pixel(cub->view, NUM_RAYS, count, 0xffff00ff);
+		count++;
+	}
+	while (count < end_y)
+	{
+		mlx_put_pixel(cub->view, NUM_RAYS, count, 0xffff70ff);
+		count++;
+	}
+	while (count < cub->map.h_px)
+	{
+		mlx_put_pixel(cub->view, NUM_RAYS, count, 0xffffff);
+	}
+
+
+
+}
+
+/*NOTAS PARA PINTAR LAS PAREDES
+
+* Usando angulos:
+-MACROS NECESARIAS:
+ANCHO DE PANTALLA
+ALTO DE PANTALLA
+TILE_SIZE Es el tamaño de cada cuadro en el mapa (PIX)
+FOV es el angulo de vision
+ROTATION_SPEED es la velocidad de rotacion
+MOV_SPEED es la velocidad de movimiento (MOV)
+
+-STRUCTS NECESARIAS:
+t_player 
+	int x;
+	int y;
+	double angle;
+	float fov_rd; //angulo de vision en radianes
+	int rotation //flag
+	int mov_left_right //flag
+	int mov_up_down //flag
+t_ray
+	double angle;
+	double dist;
+	int hit;//flag
+Variables necesarias:
+
+
+*/
